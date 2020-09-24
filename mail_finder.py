fbb389399334b58a5dc7a1e7ca5eaee60f7c9cf4
untitle <53917645+under-web@users.noreply.github.com>
2020-09-24 22:41:58 +0300

    Create mail_finder.py

---
+++ mail_finder.py
@@ -0,0 +1,32 @@
+import re
+import csv
+
+inp = 'VK_100M.txt'
+
+
+def get_reader(inp):
+    with open(inp, 'r', encoding='utf8') as file:
+        text = file.read(20000)  # указываем размер считывания из файла
+        pattern = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')  # создали шаблон
+        result = pattern.findall(text)
+        lister = []
+        s = 0
+        for i in result:
+            lister.append(i)
+            s += 1
+    print("Количество строк = ", s)
+    return lister
+
+
+def get_writer(lister):
+    with open('output.txt', 'a') as file:
+        for index in lister:
+            file.write(index + '\n')
+
+
+def main():
+    get_writer(get_reader(inp))
+
+
+if __name__ == '__main__':
+    main()
